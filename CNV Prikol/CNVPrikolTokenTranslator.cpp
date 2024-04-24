acc49d038fc8c0dfc4c73c86247a0af84c030e18
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-04-24 20:29:25 +0300

    Добавлена поддержка токена ~cnv_prikol_captain_name~

--- CNV Prikol/CNVPrikolTokenTranslator.cpp
+++ CNV Prikol/CNVPrikolTokenTranslator.cpp
@@ -4,8 +4,9 @@
 
 bool CNVPrikolTokenTranslator::TranslateToken(const char16_t* pToken, eastl::string16& dst)
 {
-	uint32_t tokenID = id(pToken);
-	if (id(u"social_credit") == tokenID/*SOCIAL_CREDIT_TOKEN.compare(pToken) == 0*/) {
+	switch (id(pToken))
+	{
+	case id(u"social_credit"):
 		if (!Simulator::IsSpaceGame())
 			dst = u"你是白痴？？？ 這不是「太空」舞台！";
 		else {
@@ -14,12 +15,33 @@ bool CNVPrikolTokenTranslator::TranslateToken(const char16_t* pToken, eastl::str
 		}
 
 		return true;
-	}
-	if (id(u"social_credit_addend") == tokenID) {
+	case id(u"social_credit_addend"):
+	{
 		auto addend = PrikolManager.GetSocialCreditAddend();
 		dst.sprintf(addend >= 0 ? u"+%d" : u"%d", addend);
-		return true;
 	}
+	return true;
+	case id(u"cnv_prikol_captain_name"):
+	{
+		ResourceKey captainKey;
+		auto empire = Simulator::GetPlayerEmpire();
+		if (Simulator::IsSpaceGame())
+			captainKey = empire->mCaptainKey;
+		else {
+			captainKey = GameNounManager.GetAvatar()->mSpeciesKey;
+		}
 
-	return false;
+		PropertyListPtr prop;
+		if (PropManager.GetPropertyList(captainKey.instanceID, 0x4062e500, prop))
+			App::Property::GetString16(prop.get(), id("editorModelNameOverride"), dst);
+		else {
+			cAssetMetadataPtr metadata;
+			Pollinator::GetMetadata(captainKey.instanceID, captainKey.groupID, metadata);
+			dst = metadata->GetName();
+		}
+	}
+	return true;
+	default:
+		return false;
+	};
 }
